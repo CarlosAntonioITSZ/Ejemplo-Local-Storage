7113b4214a813b14cf8439685ad39a7a2235a013
Jose Carlos Antonio Juarez <176w0518@itszongolica.edu.mx>
2020-03-09 11:04:26 -0600

    Ejemplo Local Storage terminado

--- assets/js/app.js
+++ assets/js/app.js
@@ -6,8 +6,12 @@ EventsListeners();
 function EventsListeners(){
     //Cuando se envia el formulario
     document.getElementById("formulario").addEventListener('submit', agregarTarea);
+    document.getElementById("caja").focus();
     //Cuando se elimna una tarea
     listaTareas.addEventListener('click', borrarTarea);
+
+    //Cargando contenido
+    document.addEventListener('DOMContentLoaded',cargarLS);
 }
 
 //Funciones
@@ -26,17 +30,21 @@ function agregarTarea(e){
     btnEliminar.classList = "btnEliminar fas fa-minus-circle";
 
     //A nuestro nuevo elemento le asignamos contenido
-    nuevaTarea.innerHTML = tarea;
+    nuevaTarea.innerHTML = tarea;    
+
     //Agregamos el boton eliminar a nuestra tarea
     nuevaTarea.appendChild(btnEliminar);
     //Agregamos nuestra nueva tarea al dom
     listaTareas.appendChild(nuevaTarea);
 
+    //Eliminos la tarea que este dentro del text area y hacemos focus
+    document.getElementById("caja").value = "";
+    document.getElementById("caja").focus();
 
     //Añadir a Local Storage
     agregarTareaLS(tarea);
 }
-
+//Agrega una tarea a Local storage
 function agregarTareaLS(tarea){
     let tareas;
     tareas = obtenerTareaLS();
@@ -44,9 +52,9 @@ function agregarTareaLS(tarea){
     tareas.push(tarea);
 
     localStorage.setItem("tarea", JSON.stringify(tareas));
-
 }
 
+//Comprueba que haya elementos en Local storage retorna un arrgelo
 function obtenerTareaLS(){
     let tareas;
 
@@ -59,10 +67,48 @@ function obtenerTareaLS(){
     return tareas;
 }
 
+function cargarLS(){
+    let tareas;
+
+    tareas = obtenerTareaLS();
+
+    tareas.forEach(function(tareas){
+     const nuevaTarea = document.createElement("li");
+    //Añadimos la clase tareas a la nueva tarea
+    nuevaTarea.classList= "tareas";
+    //Creamos el boton eliminar
+    const btnEliminar = document.createElement("i");
+    btnEliminar.classList = "btnEliminar fas fa-minus-circle";
+
+    //A nuestro nuevo elemento le asignamos contenido
+    nuevaTarea.innerHTML = tareas;
+    //Agregamos el boton eliminar a nuestra tarea
+    nuevaTarea.appendChild(btnEliminar);
+    //Agregamos nuestra nueva tarea al dom
+    listaTareas.appendChild(nuevaTarea);
+    });
+}
+
+//Elimnia tarea
 function borrarTarea(e){
     e.preventDefault();
 
     if (e.target.classList.contains("btnEliminar")){
         e.target.parentElement.remove();
+        borrarTareaLS(e.target.parentElement.textContent);
     }
+}
+//Eliminar tarea de Local storage
+function borrarTareaLS(tarea){
+    tareas = obtenerTareaLS();
+    tareaBorrar = tarea;
+
+    tareas.forEach(function(t, index){
+        if(tareaBorrar == t){
+            tareas.splice(index, 1);
+            console.log("se borro");
+        }
+    });
+
+    localStorage.setItem("tarea", JSON.stringify(tareas));
 }
